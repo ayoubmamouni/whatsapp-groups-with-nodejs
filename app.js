8062e75f340d819f568099d987f60a23ad3775ea
ayoub mamouni <47207888+ayoubmamouni@users.noreply.github.com>
2021-06-05 10:43:25 +0100

    I added some features

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+// const mongoose = require('mongoose')
+
 const app = express()
 
 //in development mode, We dont need those (dotenv & morgan) in production mode!
@@ -8,8 +10,22 @@ if (process.env.NODE_ENV !== 'production') {
     app.use(logger('dev'))
 }
 
+//connected to mongodb with mongoose
+// mongoose.connect(process.env.URI, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => console.log('Successfully connected to MongoDb'))
+//     .catch(err => console.error(err))
+
 app.set('view engine', 'ejs')
 app.use(express.static(__dirname + '/public'))
+
+//use body parser - To read informations from clinet-side
+// app.use(express.urlencoded({
+//     extended: true
+// }))
+// app.use(express.json())
 
 app.get('/', (req, res) => {
     res.render('home')
--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,7 @@ const loginForm = document.querySelector('#login-form');
 const registerBtn = document.querySelector('.register');
 const loginBtn = document.querySelector('.login');
 
+
 //close button
 const closeFormRegisterBtn = document.querySelector('.close-pop-up-form-register');
 const closeFormLoginBtn = document.querySelector('.close-pop-up-form-login');
@@ -16,3 +17,9 @@ loginBtn.addEventListener('click', () => loginForm.classList.add('show-form'))
 //close form when close btn is clicked
 closeFormRegisterBtn.addEventListener('click', () => registerForm.classList.remove('show-form'))
 closeFormLoginBtn.addEventListener('click', () => loginForm.classList.remove('show-form'))
+
+
+//scroll animation..
+const loadMoreBtn = document.querySelector('.load-more');
+const animations = document.querySelector('.animations');
+loadMoreBtn.addEventListener('click', () => animations.classList.toggle('active'))
