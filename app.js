6e5ea0989bacb82a76dc156a406f79999b394ff8
Ayoub Mamouni <ayoub.mamouni@yandex.com>
2022-04-08 12:29:23 +0000

    Readme file created.

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-const express = require('express')
-// const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
-const app = express()
+const app = express();
 
-//in development mode, We dont need those (dotenv & morgan) in production mode!
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-    const logger = require('morgan')
-    app.use(logger('dev'))
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+  const logger = require("morgan");
+  app.use(logger("dev"));
 }
 
-//connected to mongodb with mongoose
-// mongoose.connect(process.env.URI, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(() => console.log('Successfully connected to MongoDb'))
-//     .catch(err => console.error(err))
+mongoose
+  .connect(process.env.URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Successfully connected to MongoDb"))
+  .catch((err) => console.error(err));
 
-app.set('view engine', 'ejs')
-app.use(express.static(__dirname + '/public'))
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
-//use body parser - To read informations from clinet-side
-// app.use(express.urlencoded({
-//     extended: true
-// }))
-// app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
-let PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server started at ${PORT}`))
+app.get("/", (req, res) => {
+  res.render("home");
+});
+let PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started at ${PORT}`));
---
+++ readme.md
@@ -0,0 +1,4 @@
+#ARABIC PROJECT
+**This project is just front-end for now..**
+**I will work on the backend when I have time for that..**
+####Visit it later or fork it and work on it :)
